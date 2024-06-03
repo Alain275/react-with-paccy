c276eaa0a7a0b73800a250d1d83d977991da76b8
Manishimwe PACIFIQUE <mpacifique29@gmail.com>
2024-06-03 16:08:24 +0200

    new

--- reactdemo/src/components/Counter.jsx
+++ reactdemo/src/components/Counter.jsx
@@ -3,8 +3,8 @@ class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-      tages:["tag1","tage2","tage3"]
+      count: this.props.value
+      
     };
   }
 
@@ -26,55 +26,10 @@ class Counter extends Component {
     this.setState({count:this.state.count+1})
   }
 
-  render() {import React, { Component } from 'react';
-  class Counter extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        count: this.props.value
-        
-      };
-    }
-  
-   
-    
-  
-    formatCount() {
-      return this.state.count === 0 ? <div>Zero</div> : this.state.count;
-    }
-  
-    getBadge() {
-      let classes = "badge m-2 badge-";
-      classes += (this.state.count === 0) ? "warning" : "primary";
-      return classes;
-    }
-    
-    handlingIcreament =() =>{
-      
-      this.setState({count:this.state.count+1})
-    }
-  
-    render() {
-      return (
-        <div>
-         <h4>{this.props.id}</h4>
-          <span className={this.getBadge()}>{this.formatCount()}</span>
-          <button
-          onClick={this.handlingIcreament}
-            style={{ fontSize: 20 }}
-            className="btn btn-secondary btn-sm">
-            increament
-          </button>
-          
-        </div>
-      );
-    }
-  }
-  
-  export default Counter;
-  
+  render() {
     return (
       <div>
+       <h4>{this.props.id}</h4>
         <span className={this.getBadge()}>{this.formatCount()}</span>
         <button
         onClick={this.handlingIcreament}
@@ -82,7 +37,7 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm">
           increament
         </button>
-        <ul>{this.state.tages.map(tag=><li key={tag}>{tag}</li>)}</ul>
+        
       </div>
     );
   }
