ec3e0e4ab87b5592f6569351643cfb894a9cc685
Manishimwe PACIFIQUE <mpacifique29@gmail.com>
2024-06-03 15:57:41 +0200

    new

--- reactdemo/src/components/Counter.jsx
+++ reactdemo/src/components/Counter.jsx
@@ -26,7 +26,53 @@ class Counter extends Component {
     this.setState({count:this.state.count+1})
   }
 
-  render() {
+  render() {import React, { Component } from 'react';
+  class Counter extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        count: this.props.value
+        
+      };
+    }
+  
+   
+    
+  
+    formatCount() {
+      return this.state.count === 0 ? <div>Zero</div> : this.state.count;
+    }
+  
+    getBadge() {
+      let classes = "badge m-2 badge-";
+      classes += (this.state.count === 0) ? "warning" : "primary";
+      return classes;
+    }
+    
+    handlingIcreament =() =>{
+      
+      this.setState({count:this.state.count+1})
+    }
+  
+    render() {
+      return (
+        <div>
+         <h4>{this.props.id}</h4>
+          <span className={this.getBadge()}>{this.formatCount()}</span>
+          <button
+          onClick={this.handlingIcreament}
+            style={{ fontSize: 20 }}
+            className="btn btn-secondary btn-sm">
+            increament
+          </button>
+          
+        </div>
+      );
+    }
+  }
+  
+  export default Counter;
+  
     return (
       <div>
         <span className={this.getBadge()}>{this.formatCount()}</span>
