89fb15633de14e47dab9383b29af89a5372dfc22
Alain275 <alainshema275@gmail.com>
2024-06-08 09:43:25 +0200

    new

--- vidly/src/components/common/listgroup.jsx
+++ vidly/src/components/common/listgroup.jsx
@@ -1,13 +1,19 @@
+import { values } from "lodash";
 import React from "react";
 
 const ListGroup = props => {
-    const {items,textProperty,valueProperty}=props;
+    const {items,
+      textProperty,
+      valueProperty,
+      onItemSelect,
+      selectedItem}=props;
   return (
     <ul className="list-group">
       {items.map(item => (
         <li 
+        onClick={()=> onItemSelect(item)}
           key={item[valueProperty]} 
-          className="list-group-item" 
+          className={ item === selectedItem ? "list-group-item active":"list-group-item"}
          >
           {item[textProperty]}
         </li>
@@ -15,5 +21,8 @@ const ListGroup = props => {
     </ul>
   );
 };
-
+ListGroup.defaultProps = {
+  textProperty:"name",
+  valueProperty:"_id"
+}
 export default ListGroup;
--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -11,11 +11,14 @@ class Movies extends Component {
     movies: [],
     genres: [],
     pageSize: 4,
+    selectedGenre:[],
     currentPage: 1,
   };
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: getGenres() });
+    const genre = [{name:'all genres'},...getGenres()] 
+
+    this.setState({ movies: getMovies(), genres: genre });
   }
 
   handleDelete = (movie) => {
@@ -36,29 +39,30 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    // Handle genre selection
+    this.setState({selectedGenre:genre,  currentPage: 1 });
   };
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const { pageSize, currentPage,selectedGenre, movies: allMovies } = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
-
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const filteredMovies = selectedGenre && selectedGenre._id ?
+    allMovies.filter(m=>m.genre._id===selectedGenre._id):
+    allMovies;
+    const movies = paginate(filteredMovies, currentPage, pageSize);
 
     return (
       <div className="row m-4">
         <div className="col-3">
           <ListGroup 
             items={this.state.genres}
-            textProperty="name"
-            valueProperty="_id"
+            selectedItem={this.state.selectedGenre}
             onItemSelect={this.handleGenreSelect}
           />
         </div>
         <div className="col">
-          <p>Showing {count} movies in the database.</p>
+          <p>Showing {filteredMovies.length} movies in the database.</p>
           <table className="table">
             <thead>
               <tr>
@@ -93,7 +97,7 @@ class Movies extends Component {
             </tbody>
           </table>
           <Pagination
-            itemCount={count}
+            itemCount={filteredMovies.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
